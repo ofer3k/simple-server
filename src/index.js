674db16afa09fc7887fe4a47f27592ab0f64a073
ofer3k <67784636+ofer3k@users.noreply.github.com>
2021-03-06 13:02:04 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const cors=require('cors')
+const path=require('path')
+const port=process.env.PORT
+
+const app=express()
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    const indexPage=path.join(__dirname,'../public/index.html')
+    res.sendFile(indexPage)
+})
+app.listen(port,()=>{
+    console.log(`running on ${port}`)
+})
