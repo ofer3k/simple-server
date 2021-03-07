11075a3c55b6435913087721df06a1ae6f4d2a8d
ofer3k <67784636+ofer3k@users.noreply.github.com>
2021-03-07 09:23:10 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ app.get('/',(req,res)=>{
     const indexPage=path.join(__dirname,'../public/index.html')
     res.sendFile(indexPage)
 })
+app.get('/tryMe',(req,res)=>{
+    const indexPage=path.join(__dirname,'../public/tryMe.html')
+    res.sendFile(indexPage)
+})
+
 app.listen(port,()=>{
     console.log(`running on ${port}`)
 })
